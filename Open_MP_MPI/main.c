3c55d3589277ce0a0d6fd400aba5c2c8cbd2ffff
alexandrepoulain <alexandrep2@hotmail.fr>
2017-04-04 17:43:06 +0200

    debug anneau

--- Open_MP_MPI/main.c
+++ Open_MP_MPI/main.c
@@ -155,6 +155,7 @@ chained_t* cherche_calcul(chained_t* node)
   printf("bien_def = %d\n",  node->bien_def);
     printf("indice = %d\n", node->indice);
     printf("indice_fin = %d\n", node->indice_fin);
+    printf("depth = %d\n", depth);
   if(node->bien_def == 1 && node->indice < node->indice_fin-1){
       node->indice_fin--;
       return node;
