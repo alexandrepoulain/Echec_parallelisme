2dc76102bfbbe9253b6f9d4f89aee1f31ffba947
ALOUI Driss <alouidr@gmail.com>
2017-04-23 22:33:37 +0200

    debug

--- MPI_Pur2/main.c
+++ MPI_Pur2/main.c
@@ -187,7 +187,7 @@ printf("\t\t#%d cas ou moins de processus que de move\n", p);
 printf("#%d reception des result\n",p);
 			for (int i = f2+1; i < R+f2; i++) {
 				result_t child_result;
-printf("#%d reception result %i 2\n",p,i);
+printf("#%d reception result %i\n",p,i);
 				MPI_Recv(&child_result, 1, mpi_result_t, i, tag, MPI_COMM_WORLD, &status);
 printf("#%d reception result %i 2\n",p,i);
 				int child_score = -child_result.score;
