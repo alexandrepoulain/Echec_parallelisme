134d778ee2515039d7a4791079a5fbb5874d18e9
alexandrepoulain <alexandrep2@hotmail.fr>
2017-05-04 17:53:23 +0200

    debug

--- MPI_Pur/main.c
+++ MPI_Pur/main.c
@@ -148,7 +148,10 @@ void evaluate_root(tree_t * T, result_t *result, int tag, int NP, MPI_Status sta
   int job_sent = 0;
   int compt_sent = 0;
   int indice[NP];
-  int* check=calloc(NP, sizeof(int));
+  int check[NP];
+  for(int i=1;i<NP;i++)
+    check[i]=1;
+
   for(int i =0; i <n_moves; i++){
     printf("#ROOT move[%d] = %d \n", i, moves[i]);
   }
